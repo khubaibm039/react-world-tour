bcc387d18726e2eb0e1e34e27598590f19bd3775
khubaib004 <khabbab607342@gmail.com>
2023-09-11 16:08:00 +0600

    added conditional css

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -15,6 +15,9 @@ const Countries = () => {
     return (
         <div>
             <h3>Countries : {countries.length}</h3>
+            <div>
+                <h5>Visited Countries : </h5>
+            </div>
             <div className="country-container">
                 {
                     countries.map(country => <Country key={country.cca3} country={country}></Country>)
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -3,21 +3,21 @@ import "./Country.css"
 const Country = ({ country }) => {
     const { name, flags, population, area, cca3 } = country;
 
-    const [Visited, setVisited] = useState(false)
+    const [visited, setVisited] = useState(false)
     const handleVisited = () => {
-        setVisited(!Visited)
+        setVisited(!visited)
     }
 
     return (
-        <div className="country">
-            <h3>Name : {name.common} </h3>
+        <div className={`country ${visited ? 'visited' : "non-visited"}`}>
+            <h3 style={{ color: visited ? 'purple' : "white" }}> Name : {name.common} </ h3>
             <img src={flags.png} alt="" />
             <p>Population : {population}</p>
             <p>Area : {area}</p>
             <p><small>Code : {cca3}</small></p>
-            <button onClick={handleVisited}>{Visited ? 'Visited' : 'Going'}</button>
+            <button onClick={handleVisited}>{visited ? 'visited' : 'Going'}</button>
             {
-                Visited ? "I have visited this country" : " I want to visit this country"
+                visited ? "I have visited this country" : " I want to visit this country"
             }
 
         </div>
